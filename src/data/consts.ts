0439fa8a357f8bd41e4cb04b5518091abe339e9a
lvcaspacifico <lucasreinaldopacificoh@gmail.com>
2026-01-16 15:51:13 -0300

    Adds Schema + Adds Google Tag Manager (with Analytics tag) + Some refactoring to organize code better

--- src/data/consts.ts
+++ src/data/consts.ts
@@ -3,10 +3,10 @@
 // ==============================
 export const OG_IMAGE = "/og-image.png";
 export const FAVICON = "/favicon.png";
-export const LOGO = "/MH Aviation.png";
+export const LOGO = "/logo.png";
 
-export const LOGO_ASSETS = "/src/assets/brand/MH Aviation.png";
-export const LOGO_ASSETS_ALTERNATIVE = "/src/assets/brand/MH Aviation Alternative.png";
+export const LOGO_ASSETS = "/src/assets/brand/logo.png";
+export const LOGO_ASSETS_ALTERNATIVE = "/src/assets/brand/logo-anternative.png";
 export const LOGO_ASSETS_2 = "/src/assets/brand/MH Aviation White.png";
 export const LOGO_ASSETS_3 = "/src/assets/brand/MH Aviation Black.png"; 
 
